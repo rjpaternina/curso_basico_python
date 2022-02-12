2fa74a030a683fd5e78a57d1c2eed0fdf499d8da
rjpaternina <rjpaternina@gmail.com>
2022-02-11 22:58:30 -0500

    Commit day 2

--- conversor.py
+++ conversor.py
@@ -1,3 +1,54 @@
+# menu = """
+# Bienvenidos al conversor de monedas 💰
+
+# 1 - Peso colombianos
+# 2 - Pesos argentinos
+# 3 - Pesos mexicanos
+
+# Elige una opcion: """
+
+# opcion = input (menu)
+# opcion = int(opcion)
+
+# if opcion == 1:
+#     pesos = input("¡Cuantos pesos colombianos tiene?: ")
+#     pesos = float(pesos)
+#     valor_dolar = 3875
+#     dolares = pesos / valor_dolar
+#     dolares = round(dolares, 2)
+#     dolares = str(dolares)
+#     print("Tienes $" + dolares + " dolares")
+    
+# elif opcion == 2:
+#     pesos = input("¡Cuantos pesos argentinos tiene?: ")
+#     pesos = float(pesos)
+#     valor_dolar = 65
+#     dolares = pesos / valor_dolar
+#     dolares = round(dolares, 2)
+#     dolares = str(dolares)
+#     print("Tienes $" + dolares + " dolares")
+# elif opcion == 3:
+#     pesos = input("¡Cuantos pesos mexicanos tiene?: ")
+#     pesos = float(pesos)
+#     valor_dolar = 24
+#     dolares = pesos / valor_dolar
+#     dolares = round(dolares, 2)
+#     dolares = str(dolares)
+#     print("Tienes $" + dolares + " dolares")
+# else:
+#     print("Ingresa una opcion correcta")
+
+
+def conversor(tipo_pesos, valor_dolar):
+    
+    pesos = input("Cuantos pesos " + tipo_pesos + " tiene?: ")
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+    
+
 menu = """
 Bienvenidos al conversor de monedas 💰
 
@@ -11,34 +62,11 @@ opcion = input (menu)
 opcion = int(opcion)
 
 if opcion == 1:
-    pesos = input("¡Cuantos pesos colombianos tiene?: ")
-    pesos = float(pesos)
-    valor_dolar = 3875
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
-    
+    conversor("colombianos", 3875)
 elif opcion == 2:
-    pesos = input("¡Cuantos pesos argentinos tiene?: ")
-    pesos = float(pesos)
-    valor_dolar = 65
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    conversor("argentinos", 65)
 elif opcion == 3:
-    pesos = input("¡Cuantos pesos mexicanos tiene?: ")
-    pesos = float(pesos)
-    valor_dolar = 24
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    conversor("mexicanos", 24)
 else:
     print("Ingresa una opcion correcta")
-
-
-
-
 
--- funciones.py
+++ funciones.py
@@ -1,26 +1,35 @@
-# def imprimir_mensaje():
-#    print("Mensaje especial: ")
-#    print("Estoy aprendiendo a usar funciones!")
+# # def imprimir_mensaje():
+# #    print("Mensaje especial: ")
+# #    print("Estoy aprendiendo a usar funciones!")
     
 
-# imprimir_mensaje()
-# imprimir_mensaje()
-# imprimir_mensaje()
+# # imprimir_mensaje()
+# # imprimir_mensaje()
+# # imprimir_mensaje()
 
-def conversacion(mensaje):
-    print("Hola")
-    print("Como estas?")
-    print(mensaje)
-    print("Adios")
+# def conversacion(mensaje):
+#     print("Hola")
+#     print("Como estas?")
+#     print(mensaje)
+#     print("Adios")
 
-opcion = input("Elige una opcion (1, 2, 3): ")
-opcion = int(opcion)
+# opcion = input("Elige una opcion (1, 2, 3): ")
+# opcion = int(opcion)
 
-if opcion == 1:
-    conversacion("Elegiste la opcion 1")
-elif opcion == 2:
-    conversacion("Elegiste la opcion 2")
-elif opcion == 3:
-    conversacion("Elegiste la opcion 3")     
-else: 
-    print("Escribe la opcion correcta")
+# if opcion == 1:
+#     conversacion("Elegiste la opcion 1")
+# elif opcion == 2:
+#     conversacion("Elegiste la opcion 2")
+# elif opcion == 3:
+#     conversacion("Elegiste la opcion 3")     
+# else: 
+#     print("Escribe la opcion correcta")
+
+def suma(a, b):
+    print("Se suman dos numeros")
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(1, 4)
+print (sumatoria)
+    
